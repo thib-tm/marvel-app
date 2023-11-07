316692b6859217737371e5772c2fc268a9b5c606
Maillet Thibault <thibault.maillet@etu.univ-poitiers.fr>
2023-11-07 16:58:15 +0100

    Add character detail page

--- src/components/CharactersList.js
+++ src/components/CharactersList.js
@@ -1,9 +1,15 @@
-export function CharactersList({ characters }) {
+import { Link } from "react-router-dom";
+
+export function CharactersList({ characters = [] }) {
     return (
-      <ul id="characters">
-        {characters.map((character) => (
-          <li key={character.id}>{character.name}</li>
-        ))}
-      </ul>
+        <ul id="characters">
+            {characters.map((character) => (
+                <li key={character.id}>
+                    <Link to={`/characters/${character.id}`}>
+                        {character.name}
+                    </Link>
+                </li>
+            ))}
+        </ul>
     );
-  }
+}
