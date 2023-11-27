140e618be3f3c8f00edca00b9c25d28888ff16fa
Maillet Thibault <thibault.maillet@etu.univ-poitiers.fr>
2023-11-27 17:20:10 +0100

    version 1.3.0

--- src/pages/CharacterDetailPage.js
+++ src/pages/CharacterDetailPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { useLoaderData } from 'react-router';
 import CharacterDetail from '../components/CharacterDetail';
+import D3PieChart from '../components/D3PieChart';
+import RechartsPieChart from '../components/RechartsPieChart';
 
 const CharacterDetailPage = () => {
     // retrieve the character using the useLoaderData hook
@@ -12,6 +14,18 @@ const CharacterDetailPage = () => {
     return (
         <>
             <CharacterDetail character={character} />
+
+            <h2>Capacities</h2>
+            <div style={{ display: 'flex'}}>
+                <div style={{flex: '50%'}}>
+                    <h3>Using D3</h3>
+                    <D3PieChart data={character.capacities} />
+                </div>
+                <div style={{flex: '50%'}}>
+                    <h3>Using Recharts</h3>
+                    {<RechartsPieChart data={character.capacities} />}
+                </div>
+            </div>
         </>
     );
 };
